9e740352cf367155ad6fef35564dcfff6e55825c
SilentTT <8359251@qq.com>
2020-05-26 17:52:51 +0800

    Update langs.go
    fix

--- langs.go
+++ langs.go
@@ -3,16 +3,5 @@ package pelican
 // see
 // https://msdn.microsoft.com/en-us/library/windows/desktop/dd318693(v=vs.85).aspx
 func isLanguageWhitelisted(key string) bool {
-	localeID := key[:4]
-	primaryLangID := localeID[2:]
-
-	switch primaryLangID {
-	// neutral
-	case "00":
-		return true
-	// english
-	case "09":
-		return true
-	}
-	return false
+	return true
 }
